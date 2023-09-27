18804a8240bda46e46d909e71da1915c543a848b
Mateo Barbato <barbatomateo@gmail.com>
2023-09-27 16:46:10 -0300

    asdasd

--- app/page.js
+++ app/page.js
@@ -3,20 +3,48 @@ import * as fonts from '../fonts/fonts'
 export default function Home() {
   return (
     <main className="min-h-screen m-auto">
-      <div className='mx-6 self-center m-2'>
-        <button class="bg-[#43523A] hover:bg-[#43523A] text-white font-bold py-2 px-4 rounded-full animate-pulse flex space-evenly items-center gap-2">
-          <div className='w-3 h-3 '>
-            <img src='./whatsapp.png'></img>
-          </div>
-          <div className=''>
-            <p class="text-xs">Hablame a Wp!</p>
-          </div>
-        </button>
+        <div className='mx-3 self-center text-[#43523a] text-4xl text-center mt-4 mb-6'>
+            <h2 className={fonts.inconsolataVerde.className}>Proyecto Praderas</h2>
+            <h2 className={fonts.inconsolataVerde.className}>Ernestina Anchorena</h2>
+        </div>
+        <div className='mx-3 self-center m-2 grid grid-cols-2 gap-2 cols-span-1 mb-4'>
+          <a href='https://www.instagram.com/ernestinaanchorena2/?igshid=MzRlODBiNWFlZA%3D%3D' target='_blank'>
+            <button className="bg-white hover:bg-white text-[#43523a] font-bold py-2 px-4 rounded-full animate-pulse flex space-evenly items-center gap-1">
+            <div className='w-5 h-5'>
+              <img src='./ig.png'></img>
+            </div>
+            <div className='text-xs'>
+              <p className={fonts.inconsolata.className}>Ernestina Anchorena</p>
+            </div>
+          </button>
+          </a>
+          <a href='https://instagram.com/cristian.mancini71?igshid=MzRlODBiNWFlZA==' target='_blank'>
+            <button className="bg-white hover:bg-white text-[#43523a] font-bold py-2 px-4 rounded-full animate-pulse flex space-evenly items-center gap-2">
+              <div className='w-5 h-5'>
+                <img src='./ig.png'></img>
+              </div>
+              <div className='text-xs'>
+                <p className={fonts.inconsolata.className}>Cristian Mancini</p>
+              </div>
+            </button>
+          </a>
+        </div>
+        <div className='mx-3 self-center flex justify-center'>
+          <a href='' target='_blank'>
+            <button className="bg-white hover:bg-white text-[#43523a] font-bold py-2 px-10 rounded-full animate-pulse flex space-evenly items-center gap-2">
+              <div className='w-5 h-5'>
+                <img src='./wp.png'></img>
+              </div>
+              <div className='text-xs'>
+                <p className={fonts.inconsolata.className}>¡Hablemos!</p>
+              </div>
+            </button>
+          </a>
       </div>
 
       
-      <div className='mx-6 m-2/'>
-        <img src='./asd2.png'></img>
+      <div className='mx-3 my-10 flex justify-center'>
+        <img className='self-center' src='./asd2.png'></img>
       </div>
     </main>
   )
--- fonts/fonts.jsx
+++ fonts/fonts.jsx
@@ -3,16 +3,26 @@ import { Inconsolata,Merriweather_Sans} from 'next/font/google'
  
 // define your variable fonts
 const inconsolata = Inconsolata({
+    weight: '600',
+    subsets: ['latin'],
+    display: 'swap',
+  })
+  const merriweather_Sans = Merriweather_Sans({
     weight: '400',
     subsets: ['latin'],
     display: 'swap',
   })
-const merriweather_Sans = Merriweather_Sans({
-    weight: '400',
+  const inconsolataVerde = Inconsolata({
+    weight: '900',
+    subsets: ['latin'],
+    display: 'swap',
+  })
+  const merriweather_SansBold = Merriweather_Sans({
+    weight: '800',
     subsets: ['latin'],
     display: 'swap',
   })
 // define 2 weights of a non-variable font
 
  
-export { inconsolata, merriweather_Sans}
+export { inconsolata, merriweather_Sans,inconsolataVerde,merriweather_SansBold}
